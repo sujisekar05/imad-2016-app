101a1fc94ea7f14c308e44bedad59f97976886f7
sujisekar05 <sujisekar05@gmail.com>
2016-09-26 23:09:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get("/articleone",function(req,res)){
+app.get('/articleone',function(req,res)){
     res.sendFile(path.join(__dirname, 'ui', 'articleone.html')); 
-}
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
